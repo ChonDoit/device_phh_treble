255d565bfc33faeead3502d4c1821549184ebbf2
Veynamer <113140575+Veynamer@users.noreply.github.com>
2024-05-16 23:21:13 +0300

    Brightness fix for platform ums512 and ums9230
    Successful use of GSI with excellent brightness on devices with Unisoc

--- rw-system.sh
+++ rw-system.sh
@@ -1199,3 +1199,13 @@ resetprop_phh ro.config.media_vol_default 8
 if getprop ro.vendor.build.fingerprint | grep -iq -e xiaomi/renoir; then
     resetprop_phh ro.vendor.sre.enable false
 fi
+
+# Brightness fix for platform ums512 And ums9230
+if getprop ro.board.platform |grep -iq -e ums512 -e ums9230;then
+    setprop persist.sys.qcom-brightness "$(cat /sys/class/backlight/sprd_backlight/max_brightness)"
+fi
+
+# Fix for non-AMOLED Transsion devices where brightness would be dimmer than usual
+if [ -n "$(getprop ro.vendor.transsion.backlight_12bit)" ];then
+    setprop ro.vendor.transsion.backlight_hal.optimization $(getprop ro.vendor.transsion.backlight_12bit)
+fi
