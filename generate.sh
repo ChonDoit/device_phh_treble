e5898c8be6d4b11b6cd5702ebbe25af0c3c97ec5
ChonDoit <thphantomblog@gmail.com>
2024-06-15 10:27:11 -0300

    generate.sh: Clean up!

--- generate.sh
+++ generate.sh
@@ -15,37 +15,21 @@ fi
 echo 'PRODUCT_MAKEFILES := \' > AndroidProducts.mk
 
 for part in a ab;do
-	for apps in vanilla gapps foss gapps-go;do
-		for arch in arm64 arm a64;do
+	for apps in vanilla gapps ;do
+		for arch in arm64 a64;do
 			for su in yes no;do
 				apps_suffix=""
-				apps_script=""
 				apps_name=""
 				extra_packages=""
                 vndk="vndk.mk"
 		optional_base=""
 				if [ "$apps" == "gapps" ];then
 					apps_suffix="g"
-					apps_script='$(call inherit-product, device/phh/treble/gapps.mk)'
 					apps_name="with GApps"
-				fi
-				if [ "$apps" == "gapps-go" ];then
-					apps_suffix="o"
-					apps_script='$(call inherit-product, device/phh/treble/gapps-go.mk)'
-					apps_name="Go"
-				fi
-				if [ "$apps" == "foss" ];then
-					apps_suffix="f"
-					apps_script='$(call inherit-product, vendor/foss/foss.mk)'
-					apps_name="with FOSS apps"
 				fi
 				if [ "$apps" == "vanilla" ];then
 					apps_suffix="v"
-					apps_script=''
 					apps_name="vanilla"
-				fi
-				if [ "$arch" == "arm" ];then
-					vndk="vndk-binder32.mk"
 				fi
 				if [ "$arch" == "a64" ];then
 					vndk="vndk32.mk"
@@ -80,7 +64,6 @@ TARGET_GAPPS_ARCH := ${baseArch}
 include build/make/target/product/aosp_${baseArch}.mk
 \$(call inherit-product, device/phh/treble/base.mk)
 $optional_base
-$apps_script
 $rom_script
 
 PRODUCT_NAME := $target
