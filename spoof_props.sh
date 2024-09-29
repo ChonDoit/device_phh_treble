09d7c2ec586e0839441d33d1a6f2691a51632ef6
ChonDoit <42556721+ChonDoit@users.noreply.github.com>
2024-09-29 01:54:05 -0300

    Update spoof_props.sh

--- spoof_props.sh
+++ spoof_props.sh
@@ -1,14 +1,14 @@
 #!/system/bin/sh
 
-resetprop_phh persist.sys.spoof.product akita_beta
-resetprop_phh persist.sys.spoof.device akita
+resetprop_phh persist.sys.spoof.product oriole_beta
+resetprop_phh persist.sys.spoof.device oriole
 resetprop_phh persist.sys.spoof.manufacturer Google
 resetprop_phh persist.sys.spoof.brand google
-resetprop_phh persist.sys.spoof.model Pixel 8a
-resetprop_phh persist.sys.spoof.fingerprint google/akita_beta/akita:15/AP31.240617.015/12207491:user/release-keys
-resetprop_phh persist.sys.spoof.security_patch 2024-08-05
-resetprop_phh persist.sys.spoof.first_api_level 21
-resetprop_phh persist.sys.spoof.id AP31.240617.015
+resetprop_phh persist.sys.spoof.model Pixel 6
+resetprop_phh persist.sys.spoof.fingerprint google/oriole_beta/oriole:15/AP41.240823.009/12329489:user/release-keys
+resetprop_phh persist.sys.spoof.security_patch 2024-09-05
+resetprop_phh persist.sys.spoof.first_api_level 31
+resetprop_phh persist.sys.spoof.id AP41.240823.009
 resetprop_phh persist.sys.spoof.type user
 resetprop_phh persist.sys.spoof.tags release-keys
 resetprop_phh -p --delete persist.sys.spoof.auto_update
