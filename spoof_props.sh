10e7210f8bc3d89efbfe35e732605fab3c940e66
ChonDoit <42556721+ChonDoit@users.noreply.github.com>
2024-12-08 20:47:29 -0300

    Update spoof_props.sh

--- spoof_props.sh
+++ spoof_props.sh
@@ -4,7 +4,7 @@ resetprop_phh persist.sys.spoof.product oriole_beta
 resetprop_phh persist.sys.spoof.device oriole
 resetprop_phh persist.sys.spoof.manufacturer Google
 resetprop_phh persist.sys.spoof.brand google
-resetprop_phh persist.sys.spoof.model Pixel 6
+resetprop_phh persist.sys.spoof.model "Pixel 6"
 resetprop_phh persist.sys.spoof.fingerprint google/oriole_beta/oriole:15/BP11.241025.006/12620009:user/release-keys
 resetprop_phh persist.sys.spoof.security_patch 2024-11-05
 resetprop_phh persist.sys.spoof.first_api_level 31
