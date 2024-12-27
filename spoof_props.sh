b9400b5129d98f3ff3bb7e29ca6344450184c1db
ChonDoit <42556721+ChonDoit@users.noreply.github.com>
2024-12-27 10:23:30 -0300

    Update spoof_props.sh

--- spoof_props.sh
+++ spoof_props.sh
@@ -5,10 +5,10 @@ resetprop_phh persist.sys.spoof.device oriole
 resetprop_phh persist.sys.spoof.manufacturer Google
 resetprop_phh persist.sys.spoof.brand google
 resetprop_phh persist.sys.spoof.model "Pixel 6"
-resetprop_phh persist.sys.spoof.fingerprint google/oriole_beta/oriole:15/BP11.241025.006/12620009:user/release-keys
-resetprop_phh persist.sys.spoof.security_patch 2024-11-05
+resetprop_phh persist.sys.spoof.fingerprint google/oriole_beta/oriole:Baklava/BP21.241121.009/12787338:user/release-keys
+resetprop_phh persist.sys.spoof.security_patch 2024-12-05
 resetprop_phh persist.sys.spoof.first_api_level 31
-resetprop_phh persist.sys.spoof.id BP11.241025.006
+resetprop_phh persist.sys.spoof.id BP21.241121.009
 resetprop_phh persist.sys.spoof.type user
 resetprop_phh persist.sys.spoof.tags release-keys
 resetprop_phh -p --delete persist.sys.spoof.auto_update
